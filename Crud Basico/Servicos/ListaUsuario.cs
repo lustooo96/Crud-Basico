4248c8dbdbecdb8b7fac98c8483c8a78398332df
renner <rennercorrea40@gmail.com>
2022-07-29 10:59:48 -0300

    Alteração

--- Crud Basico/Servicos/ListaUsuario.cs
+++ Crud Basico/Servicos/ListaUsuario.cs
@@ -2,7 +2,7 @@
 
 namespace Crud_Basico.Servicos
 {
-    public sealed class ListaUsuario : IUsuarioRepositorio
+    public sealed class ListaUsuario 
     {
         private static object ObjetoFechado = new object();
         private static List<Usuario>? ListaUsuarios;
@@ -26,47 +26,11 @@ namespace Crud_Basico.Servicos
                 return ListaUsuarios;
             }
         }
-        
-        public static void SalvarRegistroUsuario(Usuario usuario)
-        {
-            if (usuario == null) throw new Exception("Usuario não foi informado");
-            ObterInstanciaDaListaUsuario.Add(usuario);
-        }
-
-        public static void EditarRegistroUsuario(Usuario usuario)
-        {
-            if (usuario == null) throw new Exception("Usuario não foi informado");
-            var posicaoUsuarioEditar = ListaUsuario.ObterInstanciaDaListaUsuario.FindIndex(listaUsuario => listaUsuario.Id == usuario.Id);
-            ObterInstanciaDaListaUsuario[posicaoUsuarioEditar] = usuario;
-        }
-
-        public static void DeletarRegistroUsuario(int id)
-        {
-            if (id == decimal.Zero) throw new Exception("O id não foi informado");
-            ObterInstanciaDaListaUsuario.RemoveAll(usuario => usuario.Id == id);
-        }
-
-        public static List<Usuario> ListarUsuarios()
-        {
-            return ObterInstanciaDaListaUsuario.ToList();
-        }
-
         public static int ReceberNumeroDoIdUsuario()
         {
             IdUsuario++;
             return IdUsuario;
         }
-
     }
-
-}
-
-public interface IUsuarioRepositorio
-{
-    int ReceberNumeroDoIdUsuario();
-    List<Usuario> ObterInstanciaDaListaUsuario();
-    List<Usuario> ListarUsuarios();
-    void SalvarRegistroUsuario(Usuario usuario);
-    void EditarRegistroUsuario(Usuario usuario);
-    void DeletarRegistroUsuario(int id);
+    
 }
