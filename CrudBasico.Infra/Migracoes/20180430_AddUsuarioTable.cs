a6e77588ab966bf7256b3e11fb792adb41c06afe
renner <rennercorrea40@gmail.com>
2022-08-11 13:27:09 -0300

    Alteracao

---
+++ CrudBasico.Infra/Migracoes/20180430_AddUsuarioTable.cs
@@ -0,0 +1,24 @@
+﻿using FluentMigrator;
+
+namespace test
+{
+    [Migration(20180430121800)]
+    public class AddUsuarioTable : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("usuario")
+                .WithColumn("idusuario").AsInt64().PrimaryKey().Identity()
+                .WithColumn("nome").AsString().NotNullable()
+                .WithColumn("senha").AsString().NotNullable()
+                .WithColumn("email").AsString().NotNullable()
+                .WithColumn("data_nascimento").AsDateTime().Nullable()
+                .WithColumn("data_criacao").AsDateTime().NotNullable();
+        }
+
+        public override void Down()
+        {
+            Delete.Table("usuario");
+        }
+    }
+}
---
+++ CrudBasico.Infra/Migracoes/ConexaoMigracao.cs
@@ -0,0 +1,29 @@
+﻿using FluentMigrator.Runner;
+using Microsoft.Extensions.DependencyInjection;
+using test;
+
+namespace CrudBasico.Infra.Migracoes
+{
+    public class ConexaoMigracao
+    {
+
+        private static string ConexaoString = "Server=INVENT0081\\SQLEXPRESS;Database=dados;Uid=sa;Pwd=Daniela@12;";
+        private static IServiceProvider CriarServico()
+        {
+            return new ServiceCollection()
+                .AddFluentMigratorCore()
+                .ConfigureRunner(rb => rb
+                    .AddSqlServer()
+                    .WithGlobalConnectionString(ConexaoString)
+                    .ScanIn(typeof(AddUsuarioTable).Assembly).For.Migrations())
+                .AddLogging(lb => lb.AddFluentMigratorConsole())
+                .BuildServiceProvider(false);
+        }
+
+        private static void AtualizarBancoDeDados(IServiceProvider serviceProvider)
+        {
+            var runner = serviceProvider.GetRequiredService<IMigrationRunner>();
+            runner.MigrateUp();
+        }
+    }
+}
